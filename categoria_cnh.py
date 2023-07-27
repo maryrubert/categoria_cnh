e8b0dc97775096a2495d7b5bce930ceb97551d6f
MARIANA RUBERT GRECCO <55103645+maryrubert@users.noreply.github.com>
2023-07-27 15:32:16 -0300

    Add files via upload

---
+++ categoria_cnh.py
@@ -0,0 +1,14 @@
+def categoria_cnh(rodas, pessoas=0, peso=o)
+    if 2 <= rodas => 3:
+        return print("A")
+    elif rodas >= 4:
+        if pessoas <= 8 and peso <= 3500:
+            return print("B")  
+        elif peso <= 6000:
+            return print("C")
+        elif pessoas > 8:
+            return print("D")
+        elif peso > 6000:
+            return print("E")
+    
+    else retun print("Erro na entrada de dados")
